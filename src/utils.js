814790b70daf5c4eec763438eb8c1492228ca42c
your_username <sardaralimuradali4@gmail.com>
2023-04-26 10:31:39 +0500

    adding the back url

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,6 @@
 // let BACK_END_URL="http://localhost:5000/api/v1"
-let BACK_END_URL="https://blog-back-node-mern.vercel.app/api/v1"
+// let BACK_END_URL="https://blog-back-node-mern.vercel.app/api/v1"
+let BACK_END_URL="https://blog-app-mern-back.vercel.app/api/v1"
+
 
 export {BACK_END_URL}
